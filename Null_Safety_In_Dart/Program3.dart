d994c98498425048b78ea3ded6f172ef39a0ed90
ajayayrekaraj <ajayayrekaraj@gmail.com>
2023-12-17 00:30:50 +0530

    Null Safety Codes

---
+++ Null_Safety_In_Dart/Program3.dart
@@ -0,0 +1,9 @@
+void playerInfo({int? jerNo, String? name}) {
+  print(jerNo);
+  print(name);
+}
+
+void main() {
+  playerInfo(name: "Virat", jerNo: 18);
+  playerInfo(name: "Rohit");
+}
---
+++ Null_Safety_In_Dart/Program4.dart
@@ -0,0 +1,9 @@
+void fun({int? jerNo, String? name}) {
+  print("In fun");
+}
+
+void main() {
+  int jerNo = null;
+  String? name;
+  fun(jerNo: null, name: null);
+}
